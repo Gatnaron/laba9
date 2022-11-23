8bad44b49b83dc65b8f64b7f34d36189d7a85a7d
Gatnaron <oganyan.ivan2016@yandex.ru>
2022-11-23 21:02:27 +0300

    Добавьте файлы проекта.

---
+++ laba9/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace laba9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 39;
+            this.listBox1.Location = new System.Drawing.Point(17, 16);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(512, 277);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 335);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(513, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Записать числа в файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 385);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(513, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Считать числа из файла";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 436);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(405, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Вставить число в случайную позицию в файле";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 486);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(513, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Информация о файле";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(431, 436);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(99, 41);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 537);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(513, 43);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Копировать файл";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 614);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ laba9/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace laba9
+{
+    public partial class Form1 : Form
+    {
+        enum WriteMode { Append, Rewrite}
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Функционал для того чтобы можно было в винде использовать "Открыть с помощью..."
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                var nums = ReadNumbersBinary(args[1]);
+                listBox1.Items.Clear();
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    listBox1.Items.Add((i + 1).ToString() + ".\t" + nums[i].ToString());
+                }
+            }
+            //
+        }
+        void WriteNumbersBinary(List<int> numbers, string @path, WriteMode mode)
+        //Функция для записи чисел numbers в файл с адресом path
+        //WriteMode - способ записи, есть 2 варианта:
+        //WriteMode.Append - дозаписать в файл, сохранив предыдущие значения
+        //WriteMode.Rewrite - удаляет предыдущий файл по заданному адресу, и создает на его месте новый
+        {
+            switch (mode)
+            {
+                case WriteMode.Append:
+                {
+                        if (File.Exists(path) == false)
+                            throw new Exception("Файл не существует!");
+                        else
+                        {
+                            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+                            {
+                                foreach (int number in numbers)
+                                {
+                                    writer.Write(number);
+                                }
+                            }
+                        }
+                    break;
+                }
+                case WriteMode.Rewrite:
+                {
+                    if (File.Exists(path))
+                        File.Delete(path);
+
+                    using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+                    {
+                        foreach (int number in numbers)
+                        {
+                            writer.Write(number);
+                        }
+                    }
+                    break;
+                }
+                default:
+                    throw new Exception("Не задан режим записи файла!");
+            }
+        }
+        List<int> ReadNumbersBinary(string @path) //Функция для чтения чисел из файла с адресом path
+        {
+            List<int> result = new List<int>();
+
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                while (reader.PeekChar() > -1)
+                {
+                    result.Add(reader.ReadInt32());
+                }
+                reader.Close();
+            }
+
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Задание 1: Запись чисел в файл
+        {
+            //Окно выбора адреса для файла
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Бинарный файл с числами (*.binn)|*.binn";
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            string path = saveFileDialog.FileName;
+
+            //Генерация чисел
+            List<int> nums = new List<int>();
+            Random random = new Random();
+            for(int i = 0; i < 100; i++)
+            {
+                nums.Add(random.Next(100));
+            }
+            try //Запись чисел в файл
+            {
+                WriteNumbersBinary(nums,path,WriteMode.Rewrite);
+            }
+            //Информация по итогу операции: ошибка или сообщение об успешном завершении
+            catch (Exception ex) { MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            MessageBox.Show("Файл сохранён.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Задание 1: Чтение чисел из файла
+        {
+            //Окно выбора файла для открытия
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Бинарный файл с числами (*.binn)|*.binn";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            string path = openFileDialog.FileName;
+
+            //Чтение чисел из файла в переменную nums
+            List<int> nums = new List<int>();
+            nums = ReadNumbersBinary(path);
+
+            //Выводим полученные числа в listbox
+            listBox1.Items.Clear();
+            for(int i=0; i < nums.Count;i++)
+            {
+                listBox1.Items.Add((i+1).ToString() + ".\t" + nums[i].ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Задание 2: Вставка числа на случайную позицию
+        {
+            //Окно выбора файла для открытия
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Бинарный файл с числами (*.binn)|*.binn";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            string path = openFileDialog.FileName;
+
+            //Чтение чисел из файла в переменную nums
+            List<int> nums = new List<int>();
+            nums = ReadNumbersBinary(path);
+
+            Random rnd = new Random(); //Объект рандома
+            var rn = rnd.Next(nums.Count); //Случайный номер позиции для вставки числа
+            nums.Insert(rn, (int)numericUpDown1.Value); //Вставляем число из numericUpDown в полученную на пред. строчке позицию
+            WriteNumbersBinary(nums, path, WriteMode.Rewrite); //Перезаписываем файл с учётом вставки
+
+            //Выводим информацию о вставке
+            MessageBox.Show("Число " + numericUpDown1.Value.ToString() + " вставлено на позицию " +
+                (rn+1).ToString(), "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Бинарный файл с числами (*.binn)|*.binn";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            string path = openFileDialog.FileName;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Бинарный файл с числами (*.binn)|*.binn";
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            string path2 = saveFileDialog.FileName;
+
+            File.Copy(path, path2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Бинарный файл с числами (*.binn)|*.binn";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            string path = openFileDialog.FileName;
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            MessageBox.Show("Дата и время создания файла: " + fileInfo.CreationTime.ToString() + Environment.NewLine
+                + "Дата и время последнего доступа: " + fileInfo.LastAccessTime.ToString() + Environment.NewLine
+                + "Дата и время последней записи: " + fileInfo.LastWriteTime.ToString(), "Информация о файле");
+        }
+    }
+}
